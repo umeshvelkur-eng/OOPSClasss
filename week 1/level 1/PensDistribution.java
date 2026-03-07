326d5a9c017eef9bfb7e94ff905f879630b945f8
Velkur Narendra Umesh <umeshvelkur@gmail.com>
2026-03-07 09:58:50 +0530

    week1-2

---
+++ week 1/level 1/PensDistribution.java
@@ -0,0 +1,13 @@
+public class PensDistribution {
+    public static void main(String[] args) {
+
+        int pens = 14;
+        int students = 3;
+
+        int pensPerStudent = pens / students;
+        int remaining = pens % students;
+
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                " and the remaining pen not distributed is " + remaining);
+    }
+}
