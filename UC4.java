6fd57defb18e3898f71b1cafdb533e871bc21bf8
Velkur Narendra Umesh <umeshvelkur@gmail.com>
2026-03-07 09:01:44 +0530

    uc4

---
+++ UC4.java
@@ -0,0 +1,14 @@
+public class UC4 {
+    public static void main(String[] args) {
+        String[] banner = {
+            "*******  *******  ******   *******",
+            "*     *  *     *  *     *  *      ",
+            "*     *  *     *  ******   *******",
+            "*     *  *     *  *              *",
+            "*******  *******  *        *******"
+        };
+        for(String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
